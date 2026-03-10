6993dea324db5080c7aaac4c273a682260ddacbf
Firnaz Ahamed <firnazahamed@gmail.com>
2026-03-09 20:51:54 -0700

    Fix issues

--- pages/6_🔁_Trades.py
+++ pages/6_🔁_Trades.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from settings import owner_team_dict, bucket_name
 from helpers import read_file
-from st_aggrid import AgGrid
 
 
 st.set_page_config(layout="wide")
@@ -11,4 +10,3 @@ for owner in sorted(owner_team_dict.keys()):
     trade_df = read_file(bucket_name, f"Trades/{owner}.csv").set_index("S.no")
 
     st.dataframe(trade_df)
-    # AgGrid(trade_df)
